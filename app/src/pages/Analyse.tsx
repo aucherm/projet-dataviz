0fe1e296422ede43da0752e8910319a72dd36d50
Lucas Q <lucas.queveau44190@gmail.com>
2025-11-24 11:10:56 +0100

    chore reformat

--- app/src/pages/Analyse.tsx
+++ app/src/pages/Analyse.tsx
@@ -1,7 +1,9 @@
+import { Header } from "../components/Header";
+
 export default function Analyse() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
