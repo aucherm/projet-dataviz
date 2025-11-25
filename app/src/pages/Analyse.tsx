c65c93d8c09779e280563b506530c39da410902f
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-25 12:28:37 +0100

    chore SecondGraph

--- app/src/pages/Analyse.tsx
+++ app/src/pages/Analyse.tsx
@@ -1,11 +1,13 @@
 import FirstGraph from "../components/FirstGraph";
 import { Header } from "../components/Header";
+import SecondGraph from "../components/SecondGraph";
 
 export default function Analyse() {
   return (
     <div>
       <Header />
       <FirstGraph />
+      <SecondGraph />
     </div>
   );
 }
