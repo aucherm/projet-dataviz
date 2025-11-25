3bf572b0b8cfe863b294a7e780529b09c31c90ce
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-25 10:02:13 +0100

    chore css

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -2,23 +2,24 @@ import { NavLink } from "react-router";
 
 export function Header() {
   return (
-    <header className="bg-[#BEBEBE] text-black px-10 py-4 gap-10 max-w-7xl mx-auto border-2 rounded-md">
-      <nav className="w-full px-8 py-4 flex items-center gap-6"></nav>
-      <NavLink
-        to="/"
-        className="{({ isActive }) =>
-      ` px-4 py-2 rounded-full hover:bg-black hover:text-white"
-      >
-        Accueil
-      </NavLink>
+    <header className=" text-black px-10 py-4 gap-10 mx-auto">
+      <nav className="w-full px-8 py-4 flex items-center gap-6">
+        <NavLink
+          to="/"
+          className="{({ isActive }) =>
+      ` px-4 py-2 rounded-full bg-white hover:bg-black hover:text-white"
+        >
+          Accueil
+        </NavLink>
 
-      <NavLink
-        to="/Analyse"
-        className="{({ isActive }) =>
-      ` px-4 py-2 rounded-full hover:bg-black hover:text-white"
-      >
-        Analyse
-      </NavLink>
+        <NavLink
+          to="/Analyse"
+          className="{({ isActive }) =>
+      ` px-4 py-2 rounded-full bg-white hover:bg-black hover:text-white"
+        >
+          Analyse
+        </NavLink>
+      </nav>
     </header>
   );
 }
