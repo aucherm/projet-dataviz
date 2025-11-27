32a9570ce098872b3b43a4f4f3f4e38a57d34099
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-27 14:57:13 +0100

    chore fifthgraph

--- app/src/pages/Analyse.tsx
+++ app/src/pages/Analyse.tsx
@@ -2,20 +2,24 @@ import FirstGraph from "../components/FirstGraph";
 import { Header } from "../components/Header";
 import SecondGraph from "../components/SecondGraph";
 import ThirdGraph from "../components/ThirdGraph";
+import TopRealisateursChart from "../components/FifthGraph";
 
 export default function Analyse() {
   return (
     <div className="bg-[#e8d6cc] min-h-screen ">
       <Header />
-      <div className="flex gap-6 px-6 py-4">
-        <div className="bg-white p-4 rounded-xl shadow-md w-1/3">
+      <div className="grid gap-6 px-6 py-4">
+        <div className="bg-white p-4 rounded-xl shadow-md w-1/2">
           <FirstGraph />
         </div>
-        <div className="bg-white p-4 rounded-xl shadow-md w-1/3">
+        <div className="bg-white p-4 rounded-xl shadow-md w-1/2">
           <SecondGraph />
         </div>
-        <div className="bg-white p-4 rounded-xl shadow-md w-1/3">
+        <div className="bg-white p-4 rounded-xl shadow-md w-1/2">
           <ThirdGraph />
+        </div>
+        <div className="bg-white p-4 rounded-xl shadow-md w-1/2">
+          <TopRealisateursChart />
         </div>
       </div>
     </div>
