68e2ac9782fa3b954dcaab25bfd318fe7be686ed
Lucas Q <lucas.queveau44190@gmail.com>
2025-11-27 12:20:58 +0100

    feat add fetchApi function

--- app/src/components/FifthGraph.tsx
+++ app/src/components/FifthGraph.tsx
@@ -1,3 +1,10 @@
+async function fetchApi(){
+    const api = await fetch("https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?limit=100")
+    const json = await api.json()
+
+    return json
+}
+
 export function FifthGraph(){
     return(
         <div></div>
