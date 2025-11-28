6cd5cd842e15862a7db74cf8a78802e7d15b0f62
Maëlle Aucher <maelle.aucher@gmail.com>
2025-11-28 15:45:44 +0100

    chore : responsive charts

--- app/src/pages/Analyse.tsx
+++ app/src/pages/Analyse.tsx
@@ -1,6 +1,6 @@
 import { FirstGraph } from "../components/FirstGraph";
 import { Header } from "../components/Header";
-import {SecondGraph} from "../components/SecondGraph";
+import { SecondGraph } from "../components/SecondGraph";
 import ThirdGraph from "../components/ThirdGraph";
 import { FourthGraph } from "../components/FourthGraph";
 import TopRealisateursChart from "../components/FifthGraph";
@@ -8,9 +8,10 @@ import { Footer } from "../components/Footer";
 
 export default function Analyse() {
   return (
-    <div className="bg-[#e8d6cc] min-h-screen ">
+    <div className="bg-[#e8d6cc] min-h-screen">
       <Header />
-      <div className="grid grid-cols-2 gap-6 px-6 py-4">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-6 py-4">
         <div className="bg-white p-4 rounded-xl shadow-md">
           <FirstGraph />
         </div>
@@ -27,6 +28,7 @@ export default function Analyse() {
           <TopRealisateursChart />
         </div>
       </div>
+
       <Footer />
     </div>
   );
