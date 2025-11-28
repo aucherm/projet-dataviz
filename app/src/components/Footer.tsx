7f3a93797e67b7484c24b1071dcdd49ec56ac332
Lucas Q <lucas.queveau44190@gmail.com>
2025-11-28 10:02:14 +0100

    chore : add css to footer

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer(){
     return(
-        <footer>
+        <footer className="flex justify-center items-center p-10 bg-[#F2F2E9] mt-10">
             <p>&copy; Copyright Lucas Quéveau - Maëlle Aucher - 2025</p>
         </footer>
     )
